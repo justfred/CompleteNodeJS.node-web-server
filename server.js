da4ad5ae13bd36f19d38aae64e94f450958c54d8
Frederick Heald <fheald@bandwidthx.com>
2018-02-14 17:21:21 -0800

    5.45 Middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 //new express app
 var app = express();
@@ -11,9 +12,28 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 //middleware
+
+//sample middleware - logging
+app.use((req, res, next) => {
+  const now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+//example middleware without next
+// app.use((req, res, next) => {
+//   res.render('./maintenance.hbs');
+// });
 //serve the public directory
 app.use(express.static(__dirname + '/public'));
 
+//sample hbs helpers
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
